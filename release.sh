557cfa7dc28b2fdf6065fd9e4a593b64407add7f
Martin Thomson <mt@lowentropy.net>
2022-08-04 09:51:15 +1000

    Make a release with rust-hpke as the default
    And a few other enhancements

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -ne 1 ]]; then
+   echo "Usage: $0 <version>" 1>&2
+   exit 2
+fi
+
+find . -name Cargo.toml -exec sed -i -e '/^\[package\]/,/^\[/{s/^version = ".*"/version="'"$1"'";}' {} \+
+find . -name Cargo.toml -exec git commit -m "Update version to $1" {} \+
+git tag -m "Tag release $1" "v$1"
