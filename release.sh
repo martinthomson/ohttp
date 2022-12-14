60e970c1b7541254b546f0c057a65342be717848
Martin Thomson <mt@lowentropy.net>
2022-12-14 12:22:08 +1100

    A more complete release procedure; sadly, failure recovery not included

--- release.sh
+++ release.sh
@@ -7,7 +7,16 @@ fi
 
 trap 'echo "*** release failed"; exit 1' ERR
 
+[[ -e .git/hooks/pre-commit ]] || ! echo "pre-commit hook not enabled"
+[[ -z "$(git status --short)" ]] || ! echo "uncommitted files"
+[[ "$(git rev-parse --abbrev-ref @)" == "main" ]] || ! echo "not on main branch"
+[[ "$(git rev-parse --show-toplevel)" == "$(pwd -P)" ]] || ! echo "not in repository directory"
+
 v="${1#v}"
 find . -path ./target -prune -o -name Cargo.toml -exec sed -i -e '/^\[package\]/,/^\[/{s/^version = ".*"/version = "'"$v"'"/;}' {} \+
 find . -path ./target -prune -o -name Cargo.toml -exec git commit -m "Update version to $v" {} \+
 git tag -m "Tag release $v" "v$v"
+git push origin main
+git push origin "v$v"
+cargo publish -p ohttp
+cargo publish -p bhttp
