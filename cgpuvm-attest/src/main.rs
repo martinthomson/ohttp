a0600eee780a7440f87f9138fee35a569096152a
Antoine Delignat-Lavaud <antdl@microsoft.com>
2024-08-01 13:23:14 +0000

    FFI for CVM guest attestation library

---
+++ cgpuvm-attest/src/main.rs
@@ -0,0 +1,28 @@
+use libc::{c_int, size_t};
+
+#[link(name = "azguestattestation")]
+extern {
+    fn get_attestation_token(app_data: *const u8, jwt: *mut u8,  jwt_len: *mut size_t) -> c_int;
+}
+
+pub fn attest(data: &[u8]) -> Option<Vec<u8>> {
+    unsafe {
+        let mut dstlen = 32*1024;
+        let mut dst = Vec::with_capacity(dstlen as usize);
+        let pdst = dst.as_mut_ptr();
+
+        let res = get_attestation_token(data.as_ptr(), pdst, &mut dstlen);
+        dst.set_len(dstlen as usize);
+        if res == 0 {
+            Some(dst)
+         } else {
+            None
+         }
+    }
+}
+
+pub fn main() {
+    let s = "{\"a\":1}";
+    let Some(token) = attest(s.as_bytes()) else {panic!("Failed to get MAA token")};
+    println!("Got MAA token: {}", String::from_utf8(token).unwrap());
+}
