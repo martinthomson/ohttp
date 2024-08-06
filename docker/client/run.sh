0a6f7e45c2474b288638252bce94832d87a29732
Tien Le <68622054+tle15@users.noreply.github.com>
2024-08-06 09:54:26 -0700

    updated docker files (#1)
    Co-authored-by: tienle <tienle@microsoft.co>

--- docker/client/run.sh
+++ docker/client/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Generate certificate for TLS. Todo: copy ca.crt from the server instead of running ca.sh on the client side to generate server ca.crt.
+/usr/local/bin/ca.sh
+
 # Run OHTTP client
-/usr/local/bin/ohttp-client \
+/usr/local/bin/ohttp-client --trust ./usr/local/bin/ca.crt \
   'https://localhost:9443/score' -i ./examples/request.txt \
   `curl -s -k https://localhost:9443/discover`
