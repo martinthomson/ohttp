2114f9cedd09ca4e05cf2e6aaad67722ea052d06
Kapil Vaswani <kapilv@microsoft.com>
2024-09-20 15:38:01 +0000

    KMS and MAA URLs

--- docker/server/run.sh
+++ docker/server/run.sh
@@ -8,5 +8,19 @@ fi
 # Generate certificate for TLS
 /usr/local/bin/ca.sh
 
+CMD="/usr/local/bin/ohttp-server --certificate /usr/local/bin/server.crt --key /usr/local/bin/server.key --target $TARGET"
+
+if [[ -z ${INSTANCE_SPECIFIC_KEY} ]]; then
+   CMD="$CMD --attest"
+fi
+
+if [[ ${MAA_URL} ]]; then 
+  CMD="$CMD --maa_url ${MAA_URL}"
+fi
+
+if [[ ${KMS_URL} ]]; then 
+  CMD="$CMD --kms_url ${KMS_URL}"
+fi
+
 # Run OHTTP server
-/usr/local/bin/ohttp-server --certificate /usr/local/bin/server.crt --key /usr/local/bin/server.key --target $TARGET
+`$CMD`
