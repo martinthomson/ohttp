69a22b95843b8f50f831eeaad620e670664940f6
Martin Thomson <mt@lowentropy.net>
2024-10-29 13:24:42 +1100

    Improve varint codec test coverage

--- bhttp/src/rw.rs
+++ bhttp/src/rw.rs
@@ -8,12 +8,10 @@ use crate::{err::Error, ReadSeek};
 
 #[cfg(feature = "write-bhttp")]
 #[allow(clippy::cast_possible_truncation)]
-fn write_uint(n: u8, v: impl Into<u64>, w: &mut impl io::Write) -> Res<()> {
-    let v = v.into();
-    assert!(n > 0 && usize::from(n) < std::mem::size_of::<u64>());
-    for i in 0..n {
-        w.write_all(&[((v >> (8 * (n - i - 1))) & 0xff) as u8])?;
-    }
+pub(crate) fn write_uint<const N: usize>(v: impl Into<u64>, w: &mut impl io::Write) -> Res<()> {
+    let v = v.into().to_be_bytes();
+    assert!((1..=std::mem::size_of::<u64>()).contains(&N));
+    w.write_all(&v[std::mem::size_of::<u64>() - N..])?;
     Ok(())
 }
 
@@ -21,11 +19,11 @@ fn write_uint(n: u8, v: impl Into<u64>, w: &mut impl io::Write) -> Res<()> {
 pub fn write_varint(v: impl Into<u64>, w: &mut impl io::Write) -> Res<()> {
     let v = v.into();
     match () {
-        () if v < (1 << 6) => write_uint(1, v, w),
-        () if v < (1 << 14) => write_uint(2, v | (1 << 14), w),
-        () if v < (1 << 30) => write_uint(4, v | (2 << 30), w),
-        () if v < (1 << 62) => write_uint(8, v | (3 << 62), w),
-        () => panic!("Varint value too large"),
+        () if v < (1 << 6) => write_uint::<1>(v, w),
+        () if v < (1 << 14) => write_uint::<2>(v | (1 << 14), w),
+        () if v < (1 << 30) => write_uint::<4>(v | (2 << 30), w),
+        () if v < (1 << 62) => write_uint::<8>(v | (3 << 62), w),
+        () => panic!("varint value too large"),
     }
 }
 
@@ -104,5 +102,69 @@ where
         Ok(Some(v))
     } else {
         Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::Cursor;
+
+    use super::{read_varint, write_varint};
+    use crate::{rw::read_vec, Error};
+
+    #[test]
+    fn basics() {
+        for i in [
+            0_u64,
+            1,
+            17,
+            63,
+            64,
+            100,
+            0x3fff,
+            0x4000,
+            0x1_0002,
+            0x3fff_ffff,
+            0x4000_0000,
+            0x3456_dead_beef,
+            0x3fff_ffff_ffff_ffff,
+        ] {
+            let mut buf = Vec::new();
+            write_varint(i, &mut buf).unwrap();
+            let sz_bytes = (64 - i.leading_zeros() + 2 + 7) / 8; // +2 size bits, +7 to round up
+            assert_eq!(
+                buf.len(),
+                usize::try_from(sz_bytes.next_power_of_two()).unwrap()
+            );
+
+            let o = read_varint(&mut Cursor::new(buf.clone())).unwrap();
+            assert_eq!(Some(i), o);
+
+            for cut in 1..buf.len() {
+                let e = read_varint(&mut Cursor::new(buf[..cut].to_vec())).unwrap_err();
+                assert!(matches!(e, Error::Truncated));
+            }
+        }
+    }
+
+    #[test]
+    fn read_nothing() {
+        let o = read_varint(&mut Cursor::new(Vec::new())).unwrap();
+        assert!(o.is_none());
+    }
+
+    #[test]
+    #[should_panic(expected = "varint value too large")]
+    fn too_big() {
+        _ = write_varint(0x4000_0000_0000_0000_u64, &mut Vec::new());
+    }
+
+    #[test]
+    fn too_big_vec() {
+        let mut buf = Vec::new();
+        write_varint(10_u64, &mut buf).unwrap();
+        buf.resize(10, 0); // Not enough extra for the promised length.
+        let e = read_vec(&mut Cursor::new(buf.clone())).unwrap_err();
+        assert!(matches!(e, Error::Truncated));
     }
 }
